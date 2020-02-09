d0b203719a9f0a2394eddbba5b1501d377c933dd
Middi <richard@richardmiddleton.me>
2020-02-09 17:10:39 +0000

    Add todo component

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,105 @@
 <template>
-  <div id="app">
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
-  </div>
+	<div id="app">
+		<Header />
+		<AddTodo v-on:add-todo="addTodo" />
+		<Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+	</div>
 </template>
 
 <script>
-import Todos from './components/Todos';
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import Header from "./components/layout/Header";
 
 export default {
-  name: 'App',
-  components: {
-    Todos
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
+	name: "App",
+	components: {
+		Todos,
+		AddTodo,
+		Header
+	},
+	methods: {
+		deleteTodo(id) {
+			this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      // alert('eded')
+      this.todos = [...this.todos, newTodo]
     }
-  },
-  data() {
-    return {
-      todos: [
-         {
-            "userId": 1,
-            "id": 1,
-            "title": "delectus aut autem",
-            "completed": false
-          },
-          {
-            "userId": 1,
-            "id": 2,
-            "title": "quis ut nam facilis et officia qui",
-            "completed": true
-          },
-          {
-            "userId": 1,
-            "id": 3,
-            "title": "fugiat veniam minus",
-            "completed": false
-          },
-          {
-            "userId": 1,
-            "id": 4,
-            "title": "et porro tempora",
-            "completed": true
-          },
-          {
-            "userId": 1,
-            "id": 5,
-            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-            "completed": false
-          },
-          {
-            "userId": 1,
-            "id": 6,
-            "title": "qui ullam ratione quibusdam voluptatem quia omnis",
-            "completed": true
-          },
-          {
-            "userId": 1,
-            "id": 7,
-            "title": "illo expedita consequatur quia in",
-            "completed": false
-          }
-      ]
-    }
-  }
-}
+	},
+	data() {
+		return {
+			todos: [
+				{
+					userId: 1,
+					id: 1,
+					title: "delectus aut autem",
+					completed: false
+				},
+				{
+					userId: 1,
+					id: 2,
+					title: "quis ut nam facilis et officia qui",
+					completed: true
+				},
+				{
+					userId: 1,
+					id: 3,
+					title: "fugiat veniam minus",
+					completed: false
+				},
+				{
+					userId: 1,
+					id: 4,
+					title: "et porro tempora",
+					completed: true
+				},
+				{
+					userId: 1,
+					id: 5,
+					title:
+						"laboriosam mollitia et enim quasi adipisci quia provident illum",
+					completed: false
+				},
+				{
+					userId: 1,
+					id: 6,
+					title: "qui ullam ratione quibusdam voluptatem quia omnis",
+					completed: true
+				},
+				{
+					userId: 1,
+					id: 7,
+					title: "illo expedita consequatur quia in",
+					completed: false
+				}
+			]
+		};
+	}
+};
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+}
 
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
+body {
+	font-family: Arial, Helvetica, sans-serif;
+	line-height: 1.4;
+}
+
+.btn {
+	display: inline-block;
+	border: none;
+	background: #555;
+	color: #fff;
+	padding: 7px 20px;
+	cursor: pointer;
+}
+.btn:hover {
+	background: #666;
+}
 </style>
